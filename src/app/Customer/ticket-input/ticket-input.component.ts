401b7d9413c809b86fa31283cf3d85f4e45ce639
mdmuneeb <muneebzafar999@gmail.com>
2024-04-20 23:50:22 +0500

    landing page work start

---
+++ src/app/Customer/ticket-input/ticket-input.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './ticket-input.component.html',
+  styleUrl: './ticket-input.component.scss'
+})
+export class TicketInputComponent {
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import path from 'node:path';
+import { CommonEngine } from '@angular/ssr';
+import { Component } from '@angular/core';
+import { LandingPageComponent } from './Customer/landing-page/landing-page.component';
+import { NavbarComponent } from './Customer/navbar/navbar.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "landingPageCustomer", component: LandingPageComponent},
+    {path: "navbar", component: NavbarComponent}
+    
+];
