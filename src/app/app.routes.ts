129ac0c513f201a7b6beffda56f97e181eca944e
mdmuneeb <muneebzafar999@gmail.com>
2024-04-21 20:08:32 +0500

    Signup and Sign in page complete

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,9 +4,11 @@ import { CommonEngine } from '@angular/ssr';
 import { Component } from '@angular/core';
 import { LandingPageComponent } from './Customer/landing-page/landing-page.component';
 import { NavbarComponent } from './Customer/navbar/navbar.component';
+import { LoginComponent } from './Public/Login/login/login.component';
 
 export const routes: Routes = [
     {path: "landingPageCustomer", component: LandingPageComponent},
-    {path: "navbar", component: NavbarComponent}
+    {path: "navbar", component: NavbarComponent},
+    {path: "login", component: LoginComponent}
     
 ];
